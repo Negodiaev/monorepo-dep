a7c4dd135b739fb49619c4f24327177fa9513c98
Alexander Negodiaev <alex.negodiaev@gmail.com>
2024-01-19 00:57:56 +0500

    wip: configure Vercel deployment

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -14,8 +14,18 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to server!' });
 });
 
+app.get('/api/data', (req, res) => {
+  res.send({ data: {  id: '12345', items: [{ id: '1', name: 'first' }, { id: '2', name: 'second' }] } });
+});
+
+app.get('/api/hello', (req, res) => {
+  res.send('Hello from Express.js!');
+});
+
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
+
+module.exports = app;
